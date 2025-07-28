75632ea186eaf6e7b6f49d2b2a6d6e54df15911f
Ginger Impasto <ginger.impasto@gmail.com>
2025-07-28 05:59:50 +0530

    Move LeftArea into separate component

---
+++ src/components/LeftArea.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./LudoBoard.css";
+
+const LeftArea: React.FC = () => {
+  return <div className="left-area"></div>;
+};
+
+export default LeftArea;
--- src/components/LudoBoard.tsx
+++ src/components/LudoBoard.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import './LudoBoard.css';
-import Base from './Base';
-import BoardCenter from './BoardCenter';
+import React from "react";
+import "./LudoBoard.css";
+import Base from "./Base";
+import BoardCenter from "./BoardCenter";
+import LeftArea from "./LeftArea";
 
 interface LudoBoardProps {
   diceValue: number;
@@ -13,17 +14,17 @@ const LudoBoard: React.FC<LudoBoardProps> = ({ diceValue }) => {
       <div className="ludo-board">
         {/* Center of the board - now using BoardCenter component */}
         <BoardCenter />
-        
+
         {/* Left area between red and blue bases */}
-        <div className="left-area"></div>
-        
+        <LeftArea />
+
         {/* Player bases (home areas) */}
         <Base color="red" />
         <Base color="green" />
         <Base color="yellow" />
         <Base color="blue" />
       </div>
-      
+
       <div className="game-controls">
         <div className="dice-value">Current Roll: {diceValue}</div>
       </div>
