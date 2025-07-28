e6377df27cafcb66395434e4bc6cb45514d6f507
Ginger Impasto <ginger.impasto@gmail.com>
2025-07-28 08:14:57 +0530

    Add grid to left area

--- src/components/LeftArea.tsx
+++ src/components/LeftArea.tsx
@@ -1,8 +1,21 @@
 import React from "react";
-import "./LudoBoard.css";
+import "./LeftArea.css"; // Changed import to use the new CSS file
 
 const LeftArea: React.FC = () => {
-  return <div className="left-area"></div>;
+  // Create a 6x3 grid with numbers 1-18
+  const gridItems = Array.from({ length: 18 }, (_, i) => i + 1);
+
+  return (
+    <div className="left-area">
+      <div className="left-area-grid">
+        {gridItems.map((number) => (
+          <div key={number} className="left-area-cell">
+            {number}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default LeftArea;
