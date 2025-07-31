c75bb667041d65a7bf1def8723fe8c16b605265b
Ginger Impasto <ginger.impasto@gmail.com>
2025-07-31 05:52:26 +0530

    Draw 4 pawns at appropriate locations in red arrow in board center

--- src/components/board/BoardCenter.tsx
+++ src/components/board/BoardCenter.tsx
@@ -1,12 +1,62 @@
 // BoardCenter.tsx
-import React from 'react';
-import './BoardCenter.css';
+import React from "react";
+import "./BoardCenter.css";
+import Pawn from "../game/Pawn";
 
 const BoardCenter: React.FC = () => {
   return (
     <div className="center-square">
       <div className="center-colors">
-        <div className="color-arrow red"></div>
+        <div className="color-arrow red">
+          <div
+            style={{
+              position: "absolute",
+              left: "10%", // Adjusted to position at 10% 50% of the triangle
+              top: "50%",
+              transform: "translate(-50%, -50%)",
+              width: "20%", // Adjust size as needed
+              height: "20%", // Adjust size as needed
+            }}
+          >
+            <Pawn color="red" position="board" />
+          </div>
+          <div
+            style={{
+              position: "absolute",
+              left: "10%", // Adjusted to position at 10% 50% of the triangle
+              top: "25%",
+              transform: "translate(-50%, -50%)",
+              width: "20%", // Adjust size as needed
+              height: "20%", // Adjust size as needed
+            }}
+          >
+            <Pawn color="red" position="board" />
+          </div>
+          <div
+            style={{
+              position: "absolute",
+              left: "10%", // Adjusted to position at 10% 50% of the triangle
+              top: "75%",
+              transform: "translate(-50%, -50%)",
+              width: "20%", // Adjust size as needed
+              height: "20%", // Adjust size as needed
+            }}
+          >
+            <Pawn color="red" position="board" />
+          </div>
+          <div
+            style={{
+              position: "absolute",
+              left: "25%", // Adjusted to position at 10% 50% of the triangle
+              top: "50%",
+              transform: "translate(-50%, -50%)",
+              width: "20%", // Adjust size as needed
+              height: "20%", // Adjust size as needed
+            }}
+          >
+            <Pawn color="red" position="board" />
+          </div>
+        </div>
         <div className="color-arrow green"></div>
         <div className="color-arrow yellow"></div>
         <div className="color-arrow blue"></div>
